d3a39a2e93fcddabb8c177bd7dce3fbc81144f00
Venteto <g0jtb5nre@relay.firefox.com>
2024-06-01 21:32:36 -0400

    add error pages and robots.txt

--- django_project/urls_root.py
+++ django_project/urls_root.py
@@ -20,7 +20,23 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
+from django_project.views_extra import error_400, error_403, error_404, error_500
+from django_project.views_extra import robots_txt
+
+
+handler400 = error_400
+handler403 = error_403
+handler404 = error_404  # must return status=404 for APPEND_SLASH to work
+handler500 = error_500
+
+# our admin/base_site.html override template also affects the title
+admin.site.site_title = "REAL Django Admin"   # DEFAULT = "Django site admin"
+admin.site.site_header = "Real Django Admin"  # DEFAULT = "Django Administration"
+admin.site.index_title = "Site Admin Home"    # DEFAULT = "Site Administration"
+
+
 urlpatterns = [
+    path("robots.txt", robots_txt, name="robots_txt"),
     path(settings.ADMIN_URL, admin.site.urls),
     path('', TemplateView.as_view(template_name='home.dtl'), name='home'),
 ]
